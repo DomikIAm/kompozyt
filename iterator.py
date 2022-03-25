226dc04ee8342904b37d1da812de18b9c487a0dc
JD <57227707+DomikIAm@users.noreply.github.com>
2022-03-25 12:14:28 +0100

    Add files via upload

---
+++ iterator.py
@@ -0,0 +1,18 @@
+#iterable_value = 'test'
+#iterable_obj = iter(iterable_value)
+#
+#while True:
+#    try:
+#        item = next(iterable_obj)
+#        print(item)
+#    except StopIteration:
+#        break
+print("Iretracja listy")
+l = ["robak","test","Polak"]
+for i in l:
+    print(i)
+
+print("Iteracja t")
+t = ("robak","test","polak")
+for i in t:
+    print(i)
